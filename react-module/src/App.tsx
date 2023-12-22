2757036d0f0ea667dfb01673f97dcc3745fe3d1b
Hencimiru <hencimiru12@gmail.com>
2023-12-22 11:44:28 +0900

    using vite

---
+++ react-module/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import { HomeContainer } from './Container'
+
+function App() {
+
+  return (
+    <div>
+    <HomeContainer />
+    </div>
+  )
+}
+
+export default App
---
+++ react-module/src/Container/HomeContainer/index.tsx
@@ -0,0 +1,12 @@
+import { Input } from '../../components';
+
+const HomeContainer = () => {
+
+    return (
+        <div>
+            <Input />
+        </div>
+    )
+} 
+
+export default HomeContainer;
